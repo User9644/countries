27a26ccfd30272eacd363574488debf79f00a6fa
User9644 <141528905+User9644@users.noreply.github.com>
2024-04-27 18:31:40 +0200

    .

--- js.js
+++ js.js
@@ -4,14 +4,21 @@ const countries = [
         capital: '',
         sizeWiki: 0,
         sizeMoreOrLess: 0,
-        inhabitants: 0
+        inhabitants: 1
+    },
+    {
+        name: '',
+        capital: '',
+        sizeWiki: 0,
+        sizeMoreOrLess: 0,
+        inhabitants: 2
     }
 ];
 
 function displayCountries(displaySort){
     document.getElementById('countryTable').innerHTML = '';
 
-    console.log(countries.sort((a, b) => a - b));
+    console.log(countries.sort((a, b) => a.inhabitants - b.inhabitants));
 
     console.log(countries);
 }
